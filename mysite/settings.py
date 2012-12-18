735feb4ae773b356998c2ecb3607106c6dc70587
Manuchandraprasad <manu@quadloops.com>
2012-12-18 10:08:49 +0530

    Completed Messaging System

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.flatpages',
     'polls',
     'users',
-    #'messages',
+    'messages',
+    'widget_tweaks',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
